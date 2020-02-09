06239a578fb9a64a3a5bda5f61e894fce22bbda1
Rifat Nabi <to.rifat@gmail.com>
2020-02-09 21:37:36 +1100

    Add tests for empty (matrix)

---
+++ test/matrix/empty.ts
@@ -0,0 +1,27 @@
+import test from 'ava';
+import { empty } from '../../src/matrix';
+
+test('empty - 0', t => {
+  t.deepEqual(empty(0), [[]]);
+});
+
+test('empty - 1', t => {
+  t.deepEqual(empty(1), [[0]]);
+});
+
+test('empty - 3', t => {
+  t.deepEqual(empty(3), [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]);
+});
+
+test('empty - 4', t => {
+  t.deepEqual(empty(4), [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ]);
+});
